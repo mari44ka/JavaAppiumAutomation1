4ce3aa05f5fe00f9c4f452596ce3038af627245f
mari44ka <marikostenko83@gmail.com>
2018-09-27 00:43:39 -0700

    find and click on element

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -23,7 +24,7 @@ public class FirstTest {
     capabilities.setCapability("automationName", "Appium");
     capabilities.setCapability("appPackage", "org.wikipedia");
     capabilities.setCapability("appActivity", ".main.MainActivity");
-    capabilities.setCapability("app", "/Users/Mari/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+    capabilities.setCapability("app", "/Users/Mari/Desktop/JavaAppiumAutomation1/apks/org.wikipedia.apk");
     driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
   }
   @After
@@ -35,7 +36,9 @@ public class FirstTest {
   @Test
   public void firstTest()
   {
-    System.out.println("First test run");
+    WebElement element = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+    element.click();
+    //System.out.println("First test run");
 
   }
 
