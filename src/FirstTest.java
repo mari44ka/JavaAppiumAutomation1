b25072ff57bc1e846e01009c305b5f7107b3723f
mari44ka <marikostenko83@gmail.com>
2018-09-29 00:27:42 -0700

    waitforElement added

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -40,8 +43,18 @@ public class FirstTest {
     element_to_init_search.click();
     //System.out.println("First test run");
 
-    WebElement element_to_enter_search_line = driver.findElementByXPath("//*[contains(@text,'Search…')]");
+    WebElement element_to_enter_search_line = WaitforElementPresentbyXpath("//*[contains(@text,'Search…')]", "cannot find search input", 5);
     element_to_enter_search_line.sendKeys("appium");
+
+  }
+  private WebElement WaitforElementPresentbyXpath(String xpath, String error_message, long timeoutInSeconds )
+  {
+    WebDriverWait wait = new WebDriverWait(driver,timeoutInSeconds);
+    wait.withMessage(error_message + "\n");
+    By by = By.xpath(xpath);
+    return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+
+
 
   }
 
