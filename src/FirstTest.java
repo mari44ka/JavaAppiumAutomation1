d29f3cebc0837fa3f2b74c0316673e027e7706d5
mari44ka <marikostenko83@gmail.com>
2018-10-09 15:04:51 -0700

    method Swipe_up

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -119,6 +121,35 @@ public class FirstTest {
 
   }
 
+  @Test
+
+  public void testSwipeArticle()
+  {
+    waitforElementAndClick(By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+            "cannot find search input",
+            5);
+
+    waitforElementAndSendKeys(By.xpath("//*[contains(@text,'Search…')]"),
+            "Java",
+            "cannot find search input",
+            5);
+    waitforElementAndClick(
+            By.xpath("//*[@resource-id ='org.wikipedia:id/page_list_item_container']//*[@text ='Object-oriented programming language']"),
+            "cannot find Object-oriented programming language. Topic searching by java",
+            5);
+    WaitforElementPresent(By.id("org.wikipedia:id/view_page_title_text"),
+            "cannot find title",
+            10);
+
+    swipeUp(6000);
+
+
+
+
+
+  }
+
+
   private WebElement WaitforElementPresent(By by, String error_message, long timeoutInSeconds )
   {
     WebDriverWait wait = new WebDriverWait(driver,timeoutInSeconds);
@@ -164,6 +195,19 @@ public class FirstTest {
     WebElement element = WaitforElementPresent(by, error_message, timeoutInSeconds);
     element.clear();
     return element;
+
+  }
+
+  protected void swipeUp(int timeOfSwipe)
+  {
+    TouchAction action = new TouchAction(driver);
+    Dimension size = driver.manage().window().getSize();
+    int x =size.width/2;
+    int start_y = (int)(size.height * 0.8);
+    int end_y =(int)(size.height*0.2);
+    action.press(x,start_y).waitAction(timeOfSwipe).moveTo(x,end_y).release().perform();
+
+
 
   }
 
